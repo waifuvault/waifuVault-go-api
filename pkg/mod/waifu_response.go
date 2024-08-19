084e398ec6afd36f5b376d8d08e311d4a03409d2
VictoriqueMoe <loli@victorique.moe>
2024-08-19 22:35:21 +0100

    add bucket to file response

--- pkg/mod/waifu_response.go
+++ pkg/mod/waifu_response.go
@@ -16,4 +16,7 @@ type WaifuResponse[T string | int] struct {
 	// when the file will expire, if called with `format` true, then
 	// this will be a string like "332 days 7 hours 18 minutes 8 seconds"
 	RetentionPeriod T `json:"retentionPeriod"`
+
+	// Bucket - the bucket this belongs to
+	Bucket string `json:"bucket"`
 }
